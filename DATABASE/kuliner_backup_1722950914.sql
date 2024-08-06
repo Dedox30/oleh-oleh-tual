4160c15c0b1808bfee6bd9e1bf946073d7e3167d
Dedy Sahuleka <dedysahuleka229@gmail.com>
2024-08-06 23:15:16 +0900

    update database backup

--- DATABASE/kuliner_backup_1722950914.sql
+++ DATABASE/kuliner_backup_1722950914.sql
@@ -38,14 +38,15 @@ CREATE TABLE `customer` (
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `telp` varchar(200) NOT NULL,
+  `role` enum('user','admin') NOT NULL DEFAULT 'user',
   PRIMARY KEY (`kode_customer`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO customer VALUES("C0002","Rafi Akbar","a.rafy@gmail.com","rafi","$2y$10$/UjGYbisTPJhr8MgmT37qOXo1o/HJn3dhafPoSYbOlSN1E7olHIb.","0856748564");
-INSERT INTO customer VALUES("C0003","Nagita Silvana","bambang@gmail.com","Nagita","$2y$10$47./qEeA/y3rNx3UkoKmkuxoAtmz4ebHSR0t0Bc.cFEEg7cK34M3C","087804616097");
-INSERT INTO customer VALUES("C0004","Nadiya","nadiya@gmail.com","nadiya","$2y$10$6wHH.7rF1q3JtzKgAhNFy.4URchgJC8R.POT1osTAWmasDXTTO7ZG","0898765432");
-INSERT INTO customer VALUES("C0005","Agus","agus@gmail.com","agus","$2y$10$nMpGeqG9bvq7HTnHR6TKjeTKkv7vOcXdaWtVsGT.YRzWSf671MiEO","+62812345678");
-INSERT INTO customer VALUES("C0006","Dedy Sahuleka","dora@gmail.com","dedox","$2y$10$h1hNbKcWOp7c9617QBtNwe/45RdAW8WkUoeAKLcJkrZRLHH1b4osC","0812459734157");
+INSERT INTO customer VALUES("C0002","Rafi Akbar","a.rafy@gmail.com","rafi","$2y$10$/UjGYbisTPJhr8MgmT37qOXo1o/HJn3dhafPoSYbOlSN1E7olHIb.","0856748564","user");
+INSERT INTO customer VALUES("C0003","Nagita Silvana","bambang@gmail.com","Nagita","$2y$10$47./qEeA/y3rNx3UkoKmkuxoAtmz4ebHSR0t0Bc.cFEEg7cK34M3C","087804616097","user");
+INSERT INTO customer VALUES("C0004","Nadiya","nadiya@gmail.com","nadiya","$2y$10$6wHH.7rF1q3JtzKgAhNFy.4URchgJC8R.POT1osTAWmasDXTTO7ZG","0898765432","user");
+INSERT INTO customer VALUES("C0005","Agus","agus@gmail.com","agus","$2y$10$nMpGeqG9bvq7HTnHR6TKjeTKkv7vOcXdaWtVsGT.YRzWSf671MiEO","+62812345678","user");
+INSERT INTO customer VALUES("C0006","Dedy Sahuleka","dora@gmail.com","dedox","$2y$10$h1hNbKcWOp7c9617QBtNwe/45RdAW8WkUoeAKLcJkrZRLHH1b4osC","0812459734157","user");
 
 
 
@@ -59,11 +60,11 @@ CREATE TABLE `inventory` (
   PRIMARY KEY (`kode_bk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO inventory VALUES("M0001","Tepung","40","Kg","1000","2020-07-26");
-INSERT INTO inventory VALUES("M0002","Pengembang","-6","Kg","1000","2020-07-27");
-INSERT INTO inventory VALUES("M0003","Cream","5","Kg","3000","2020-07-26");
-INSERT INTO inventory VALUES("M0004","Keju","55","Kg","4000","2020-07-26");
-INSERT INTO inventory VALUES("M0005","Coklat","0","Kg","5000","2020-07-27");
+INSERT INTO inventory VALUES("M0001","Embal","9999963","pcs","10000","2024-08-06");
+INSERT INTO inventory VALUES("M0002","Enbal Crispy","899984","pcs","25000","2024-08-06");
+INSERT INTO inventory VALUES("M0003","Enbal Semprong","9999983","pcs","20000","2024-08-06");
+INSERT INTO inventory VALUES("M0004","Enbal Stick Rula","99999972","pcs","25000","2024-08-06");
+INSERT INTO inventory VALUES("M0005","Enbal Stick","899990","pcs","25000","2024-08-06");
 
 
 
@@ -75,10 +76,16 @@ CREATE TABLE `keranjang` (
   `qty` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
   PRIMARY KEY (`id_keranjang`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO keranjang VALUES("16","C0003","P0002","Maryam","5","15000");
 INSERT INTO keranjang VALUES("17","C0003","P0003","Kue tart coklat","2","100000");
+INSERT INTO keranjang VALUES("40","C0090","","","1","0");
+INSERT INTO keranjang VALUES("55","C0007","P0001","Enbal","1","10000");
+INSERT INTO keranjang VALUES("56","C00010","P0001","Enbal","1","10000");
+INSERT INTO keranjang VALUES("57","C00090","P0001","Enbal","1","10000");
+INSERT INTO keranjang VALUES("58","C00091","P0001","Enbal","3","10000");
+INSERT INTO keranjang VALUES("75","C0006","P0001","Enbal","2","10000");
 
 
 
@@ -105,8 +112,6 @@ INSERT INTO produk VALUES("P0006","Piece Enbal","669ca8e777c7f.jpg","								Enb
 									","25000");
 INSERT INTO produk VALUES("P0007","Enbal Kacang","669ca8f820bd0.jpg","								enbal dengan varian rasa kacang
 									","20000");
-INSERT INTO produk VALUES("P0008","Enbal Crispy","669b69a20c65a.jpg","Enbal dengan rasa varian crispy
-			","25000");
 
 
 
@@ -128,23 +133,32 @@ CREATE TABLE `produksi` (
   `tolak` varchar(200) NOT NULL,
   `cek` int(11) NOT NULL,
   PRIMARY KEY (`id_order`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO produksi VALUES("8","INV0001","C0002","P0003","Kue tart coklat","1","100000","Pesanan Baru","2020-07-27","Jawa Timur","Surabaya","Jl.Tanah Merah Indah 1","60129","2","1","0");
-INSERT INTO produksi VALUES("9","INV0002","C0002","P0001","Roti Sobek","3","10000","0","2020-07-27","Jawa Barat","Bandung","Jl.Jati Nangor Blok C, 10","30712","1","0","1");
-INSERT INTO produksi VALUES("10","INV0003","C0003","P0002","Maryam","2","15000","0","2020-07-27","Jawa Tengah","Yogyakarta","Jl.Malioboro, Blok A 10D","30123","1","0","0");
-INSERT INTO produksi VALUES("11","INV0003","C0003","P0003","Kue tart coklat","1","100000","0","2020-07-27","Jawa Tengah","Yogyakarta","Jl.Malioboro, Blok A 10D","30123","1","0","0");
-INSERT INTO produksi VALUES("12","INV0003","C0003","P0001","Roti Sobek","1","10000","0","2020-07-27","Jawa Tengah","Yogyakarta","Jl.Malioboro, Blok A 10D","30123","1","0","0");
-INSERT INTO produksi VALUES("13","INV0004","C0004","P0002","Maryam","1","15000","0","2020-07-26","Jawa Timur","Sidoarjo","Jl.KH Syukur Blok C 18 A","50987","1","0","0");
-INSERT INTO produksi VALUES("14","INV0005","C0005","P0001","Roti Sobek","1","10000","Pesanan Baru","2323-07-15","Jawa Barat","Bandung","Ujung Berung","40397","0","0","1");
 INSERT INTO produksi VALUES("15","INV0006","C0006","P0004","Embal","1","10000","Pesanan Baru","2424-07-11","maluku","ambon","kelurahan benteng","97111","2","1","0");
 INSERT INTO produksi VALUES("16","INV0007","C0006","P0004","Embal","2","10000","Pesanan Baru","2424-07-11","Maluku Utara","Ternate","Ternate","95111","2","1","0");
 INSERT INTO produksi VALUES("17","INV0008","C0006","P0004","Embal","2","10000","Pesanan Baru","2424-07-20","Maluku","Ambon","OSM","97112","2","1","0");
-INSERT INTO produksi VALUES("18","INV0009","C0006","P0002","Maryam","3","15000","0","2424-07-20","Maluku","Ambon","OSM","97112","1","0","1");
+INSERT INTO produksi VALUES("18","INV0009","C0006","P0002","Maryam","3","15000","0","2424-07-20","Maluku","Ambon","OSM","97112","1","0","0");
 INSERT INTO produksi VALUES("19","INV0010","C0006","P0002","Enbal Crispy","1","25000","0","2424-07-23","Maluku","Ambon","karpan","97112","1","0","0");
 INSERT INTO produksi VALUES("20","INV0011","C0006","P0006","Piece Enbal","1","25000","0","2424-07-24","Maluku","Ambon","karpan","97112","1","0","0");
 INSERT INTO produksi VALUES("21","INV0011","C0006","P0002","Enbal Crispy","1","25000","0","2424-07-24","Maluku","Ambon","karpan","97112","1","0","0");
 INSERT INTO produksi VALUES("22","INV0011","C0006","P0007","Enbal Kacang","1","20000","0","2424-07-24","Maluku","Ambon","karpan","97112","1","0","0");
+INSERT INTO produksi VALUES("23","INV0012","C0006","P0001","Enbal","4","10000","0","2424-07-24","Maluku","Ambon","Batu Merah","97814","1","0","0");
+INSERT INTO produksi VALUES("24","INV0012","C0006","P0002","Enbal Crispy","2","25000","0","2424-07-24","Maluku","Ambon","Batu Merah","97814","1","0","0");
+INSERT INTO produksi VALUES("25","INV0012","C0006","P0006","Piece Enbal","1","25000","0","2424-07-24","Maluku","Ambon","Batu Merah","97814","1","0","0");
+INSERT INTO produksi VALUES("26","INV0012","C0006","P0007","Enbal Kacang","3","20000","0","2424-07-24","Maluku","Ambon","Batu Merah","97814","1","0","0");
+INSERT INTO produksi VALUES("27","INV0013","C0006","P0003","Enbal Semprong","1","20000","0","2424-07-24","Maluku","Ambon","Batu Merah","97814","1","0","0");
+INSERT INTO produksi VALUES("28","INV0014","C0006","P0005","Enbal Stick","1","25000","Pesanan Baru","2424-07-24","Maluku","Ambon","Mangga Dua","97112","2","1","0");
+INSERT INTO produksi VALUES("29","INV0015","C0006","","","1","0","Pesanan Baru","2424-07-24","Maluku","1","1","1","0","0","0");
+INSERT INTO produksi VALUES("30","INV0015","C0006","","","1","0","Pesanan Baru","2424-07-24","Maluku","1","1","1","0","0","0");
+INSERT INTO produksi VALUES("31","INV0016","C0006","P0008","gula gula","1","1200","Pesanan Baru","2424-07-31","Ambon","Ambon","Jln dr malaiholo","97111","0","0","0");
+INSERT INTO produksi VALUES("32","INV0016","C0006","P0005","Enbal Stick","1","25000","Pesanan Baru","2424-07-31","Ambon","Ambon","Jln dr malaiholo","97111","0","0","0");
+INSERT INTO produksi VALUES("33","INV0016","C0006","P0004","Enbal Stick Rula","1","25000","Pesanan Baru","2424-07-31","Ambon","Ambon","Jln dr malaiholo","97111","0","0","0");
+INSERT INTO produksi VALUES("34","INV0017","C0006","P0002","Enbal Crispy","1","25000","0","2424-08-06","Ambon","Ambon","Jln dr malaiholo","97111","1","0","0");
+INSERT INTO produksi VALUES("35","INV0017","C0006","P0003","Enbal Semprong","1","20000","0","2424-08-06","Ambon","Ambon","Jln dr malaiholo","97111","1","0","0");
+INSERT INTO produksi VALUES("36","INV0017","C0006","P0004","Enbal Stick Rula","1","25000","0","2424-08-06","Ambon","Ambon","Jln dr malaiholo","97111","1","0","0");
+INSERT INTO produksi VALUES("37","INV0017","C0006","P0005","Enbal Stick","1","25000","0","2424-08-06","Ambon","Ambon","Jln dr malaiholo","97111","1","0","0");
+INSERT INTO produksi VALUES("38","INV0017","C0006","P0001","Enbal","2","10000","0","2424-08-06","Ambon","Ambon","Jln dr malaiholo","97111","1","0","0");
 
 
 
