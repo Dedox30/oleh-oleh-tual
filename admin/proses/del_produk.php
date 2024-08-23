9b06d9399939914cd27b032683949bdf7b694b23
Dedy Sahuleka <dedysahuleka229@gmail.com>
2024-08-23 17:59:33 +0900

    perbaikan bug pada fitur delete produk

--- admin/proses/del_produk.php
+++ admin/proses/del_produk.php
@@ -1,20 +1,51 @@
 <?php 
 include '../../koneksi/koneksi.php';
 
-$kode = $_GET['kode'];
-$produk = mysqli_query($conn, "SELECT * FROM produk where kode_produk ='$kode'");
+// Mengambil data kode yang dikirim melalui POST
+$kode = $_POST['kode'];
+
+// Mencegah SQL Injection
+$kode = mysqli_real_escape_string($conn, $kode);
+
+// Ambil data produk berdasarkan kode
+$produk = mysqli_query($conn, "SELECT * FROM produk WHERE kode_produk = '$kode'");
 $row = mysqli_fetch_assoc($produk);
-unlink("../../image/produk/".$row['image']);
-mysqli_query($conn, "DELETE FROM bom_produk where kode_produk = '$kode'");
+
+// Hapus file gambar produk jika ada
+if ($row && file_exists("../../image/produk/" . $row['image'])) {
+    unlink("../../image/produk/" . $row['image']);
+} else {
+    echo "
+    <script>
+    alert('File gambar tidak ditemukan atau data produk tidak ada');
+    window.location = '../m_produk.php';
+    </script>
+    ";
+    exit;
+}
+
+// Hapus data dari tabel bom_produk dan produk
+mysqli_query($conn, "DELETE FROM bom_produk WHERE kode_produk = '$kode'");
 $del = mysqli_query($conn, "DELETE FROM produk WHERE kode_produk = '$kode'");
 
-if($del){
-	echo "
-	<script>
-	alert('DATA BERHASIL DIHAPUS');
-	window.location = '../m_produk.php';
-	</script>
-	";
+// Periksa apakah ada baris yang terhapus
+if(mysqli_affected_rows($conn) > 0){
+    echo "
+    <script>
+    alert('DATA BERHASIL DIHAPUS');
+    window.location = '../m_produk.php';
+    </script>
+    ";
+} else {
+    // Tampilkan pesan error jika penghapusan gagal
+    echo "
+    <script>
+    alert('GAGAL MENGHAPUS DATA');
+    window.location = '../m_produk.php';
+    </script>
+    ";
+    echo "Error: " . mysqli_error($conn);
+    exit;
 }
 
 ?>
